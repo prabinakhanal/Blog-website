d91fe4f7fb230e232dc6856fd00e7eaec4100588
prabina.khanal@introcept.co <prabina.khanal@introcept.co>
2022-06-15 22:09:20 +0545

    changes in pagination part and design

--- blog.js
+++ blog.js
@@ -1,10 +1,19 @@
 const toogler = document.getElementById('toogler');
 const box = document.getElementById('box');
+const search=document.getElementById('search')
 
 
 
 toogler.addEventListener("click",() => {
 box.classList.remove('sm:hidden');
 toogler.classList.add('hidden');
+})
+
+
+
+search.addEventListener("input",(e)=>{
+const value=e.target.value
+console.log(value)
+
 })
 
